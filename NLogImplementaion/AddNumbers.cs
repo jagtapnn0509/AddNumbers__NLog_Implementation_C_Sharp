169ea9d4c11d88b5e9b989dcf2f87076469718b9
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-07 10:26:16 +0530

    Add : Function to add Two nos using NLog Library

---
+++ NLogImplementaion/AddNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogImplementaion
+{
+    class AddNumbers
+    {
+        NLog nlog = new NLog();
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nlog.LogDebug("Debug Successfully : sum()");
+                nlog.LogError("Expecting null values");
+                nlog.LogWarn("a or b should not equal to 0");
+            }
+            int c = a + b;
+            nlog.LogDebug("Debug Successfully");
+            return c;
+        }
+    }
+}
